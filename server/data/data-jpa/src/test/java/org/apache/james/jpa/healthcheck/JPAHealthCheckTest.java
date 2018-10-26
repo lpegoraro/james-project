628b425e9be541a7ef7ffa5b2131d5bf3a31e62f
Luiz Henrique Pegoraro <pegorarohluiz@gmail.com>
2018-10-26 06:17:43 -0300

    Update code review

--- server/data/data-jpa/src/test/java/org/apache/james/jpa/healthcheck/JPAHealthCheckTest.java
+++ server/data/data-jpa/src/test/java/org/apache/james/jpa/healthcheck/JPAHealthCheckTest.java
@@ -1,17 +1,16 @@
 package org.apache.james.jpa.healthcheck;
 
+import javax.persistence.EntityManagerFactory;
 import org.apache.james.backends.jpa.JpaTestCluster;
 import org.apache.james.core.healthcheck.Result;
+import org.apache.james.core.healthcheck.ResultStatus;
 import org.apache.james.mailrepository.jpa.JPAUrl;
 import org.apache.openjpa.persistence.EntityManagerFactoryImpl;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
-import javax.persistence.EntityManagerFactory;
+import static org.assertj.core.api.Assertions.assertThat;
 
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.core.Is.is;
-import static org.hamcrest.core.IsEqual.equalTo;
 
 @ExtendWith(JPAHealthCheckExtension.class)
 class JPAHealthCheckTest {
@@ -22,13 +21,19 @@ class JPAHealthCheckTest {
     void testWhenActive() {
         final JpaTestCluster JPA_TEST_CLUSTER = JpaTestCluster.create(JPAUrl.class);
         jpaHealthCheck = new JPAHealthCheck(JPA_TEST_CLUSTER.getEntityManagerFactory());
-        assertThat("Health check should be healthy", jpaHealthCheck.check(), is(equalTo(Result.healthy(jpaHealthCheck.componentName()))));
+        Result result = jpaHealthCheck.check();
+        ResultStatus healthy = ResultStatus.HEALTHY;
+        assertThat(result.getStatus()).as("Result %s status should be %s", result.getStatus(), healthy)
+                .isEqualTo(healthy);
     }
 
     @Test
     void testWhenInactive() {
         jpaHealthCheck = new JPAHealthCheck((EntityManagerFactory)
                 new EntityManagerFactoryImpl().getBrokerFactory());
-        assertThat("Health check should be healthy", jpaHealthCheck.check(), is(equalTo(Result.unhealthy(jpaHealthCheck.componentName()))));
+        Result result = jpaHealthCheck.check();
+        ResultStatus unhealthy = ResultStatus.UNHEALTHY;
+        assertThat(result.getStatus()).as("Result %s status should be %s", result.getStatus(), unhealthy)
+                .isEqualTo(unhealthy);
     }
 }
